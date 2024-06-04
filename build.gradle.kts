795af83d9e262f7498efd6f4360c73367df41349
Massimo Cantú <massimocantugarcia@gmail.com>
2024-06-03 22:49:58 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -136,7 +136,7 @@ java {
     }
 }
 
-val changelogFile = file("temp/changelog.txt")
+val changelogFile = file("build/changelog.txt")
 
 curseforge {
     val curseApiKey = optionalProperty("curseApiKey") ?: System.getenv("CURSEFORGE_API_KEY")
@@ -160,6 +160,8 @@ curseforge {
 
         mainArtifact(tasks.findByName("jar")) {
             relations {
+                requiredDependency("dynamictrees")
+                requiredDependency("pams-harvestcraft-2-trees")
                 optionalDependency("dynamictreesplus")
             }
         }
